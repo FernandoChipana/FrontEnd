c23a0876defa581a6113df167ec032643cf8f5e1
FernandoChipana <f3rm1ch3@hmail.com>
2021-06-13 12:01:48 -0500

    13/06/2021 12:02

--- src/app/components/turno/turno.component.ts
+++ src/app/components/turno/turno.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal } from'@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-turno',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurnoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal:NgbModal) { }
 
   ngOnInit(): void {
   }
